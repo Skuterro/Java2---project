c4bbe9b4382e6ba895def8636d35d243bb18b61d
Skuterro <kacperkk129@gmail.com>
2024-11-04 19:20:25 +0100

    cosmetic navbar changes

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,8 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { FiUser } from "react-icons/fi";
-import { FaDropbox } from "react-icons/fa6";
+import { FaBoxOpen } from "react-icons/fa";
 import { IconType } from "react-icons";
+import { FiBox } from "react-icons/fi";
+import { FiMessageSquare } from "react-icons/fi";
+import { IoCreateOutline } from "react-icons/io5";
 
 export const Navbar = () => {
 
@@ -15,16 +18,16 @@ export const Navbar = () => {
   return (
     <nav className="bg-black w-full flex items-center justify-between h-[5vh]">
       <div className="flex items-center ml-5">
-        <FaDropbox className="text-purple-500 mr-2 text-4xl"/>
+        <FaBoxOpen className="text-purple-500 mr-2 text-4xl"/>
         <div className="flex flex-col text-3xl">
           <span className="select-none text-white font-bold -mb-2">BLOW</span>
           <span className="select-none text-white text-sm tracking-wider">CASE</span>
         </div>
       </div>
       <ul className="bg-black flex justify-center items-center text-lg gap-[20vh] -ml-[13vh]">
-        <NavItem to="/" text="CREATE CASE" icon={FaDropbox} />
-        <NavItem to="/" text="CASES" icon={FaDropbox}/>
-        <NavItem to="/" text="CONTACT" icon={FaDropbox}/>
+        <NavItem to="/" text="CREATE CASE" icon={IoCreateOutline} />
+        <NavItem to="/" text="CASES" icon={FiBox}/>
+        <NavItem to="/" text="CONTACT" icon={FiMessageSquare}/>
       </ul>
       <div className="flex mr-5">
         <button 
@@ -51,12 +54,12 @@ export const NavItem = ({to, text, icon:Icon}: NavItemProps) => {
       to={to}
       className={({ isActive }) =>
         isActive
-        ? "flex px-2 text-white font-bold text-xl hover:text-purple-500 transition-colors duration-300 ease-in-out"
+        ? "flex items-center px-2 text-white font-bold text-xl hover:text-purple-500 transition-colors duration-300 ease-in-out"
         : "flex px-2 text-gray-600 hover:text-black transition-colors duration-500 ease-in-out"
       }
     >
-      <Icon className="text-2xl"/>
       {text}
+      <Icon className="text-2xl ml-2"/>
     </NavLink>
   );
 };
