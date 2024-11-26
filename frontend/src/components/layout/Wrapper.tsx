ce61517c309dc2faf5c8e14f68f0381599c0d1ac
Kacper <kacperkk129@gmail.com>
2024-11-26 16:16:49 +0100

    frontend

--- frontend/src/components/layout/Wrapper.tsx
+++ frontend/src/components/layout/Wrapper.tsx
@@ -6,7 +6,6 @@ interface WrapperProps {
 
 export const Wrapper = ({children}: WrapperProps) => {
   return <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mb-6">
-
     {children}
   </div>
 }
